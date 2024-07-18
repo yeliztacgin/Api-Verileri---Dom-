331053677833f1b756d5fedbf661d6a2045ce1e1
yeliztacgin <145569090+yeliztacgin@users.noreply.github.com>
2024-07-18 10:45:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const postlistDOM=document.querySelector(".post-list");
+const postUI=(posts)=>{
+   let result="";
+   posts.forEach(post => {
+      result+=
+     `
+     <li class="post-item">
+<div class="post-body">
+<span class="post-id">Post Id:<b>${post.id}</b></span>
+<strong class="post-title">${post.title}</strong>
+<p class="post-content">${post.body}</p>
+</div>
+<button class="post-button">Read More</button>
+</li>
+     
+     `;
+     postlistDOM.innerHTML=result;
+   });
+
+};
+//ajax//
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+// xhr.onload = function() {
+//   const response=xhr.responseText;
+//   const data=JSON.parse(response);
+//   postUI(data);
+// }
+
+// xhr.send();
+
+
+
+//fetch//
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//       .then(response => response.json())
+//       .then((data) => postUI(data))
+
+//async-await//
+
+
+const getData=async()=>{ 
+  const response=await  fetch("https://jsonplaceholder.typicode.com/posts");
+
+const data=await response.json();
+postUI(data);
+};
+getData();
